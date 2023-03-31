c3ba5401e5ca15feb92aaa17e41b586b6f7a50aa
kuzhelga <kuzhelga81@gmail.com>
2023-03-31 19:21:48 +0300

    changed function get(array...) to cover by test_get #2

--- utils/arrs.py
+++ utils/arrs.py
@@ -11,10 +11,10 @@ def get(array, index, default=None):
     :param default: значение по-умолчанию.
     :return: значение по индексу или значение по-умолчанию.
     """
-    if index < 0:
-        return default
+    if 0 <= index < len(array):
+        return array[index]
 
-    return array[index]
+    return default
 
 
 def my_slice(coll, start=0, end=None):
